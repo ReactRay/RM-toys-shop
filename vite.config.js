df0326d55e2efa3fce57c05d41ed72352f4d1640
ReactRay <raydiaz1899@gmail.com>
2025-01-01 18:37:56 +0200

    font change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import sass from 'sass'
 
 export default defineConfig({
+  base: '/RM-toys-shop/', // Replace with your repository name
   plugins: [react()],
   css: {
     preprocessorOptions: {
